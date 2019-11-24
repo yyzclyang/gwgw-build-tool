ee6a3aa793b9f82229173f0969cdea0eb1cf951c
yyzcl <yyzclyang@gmail.com>
2019-11-24 13:50:40 +0800

    feat：如果离上一次构建的时间超过 3 天，就强制重新构建

--- src/main.ts
+++ src/main.ts
@@ -49,8 +49,16 @@ const projectShouldBuild = (
     () => gitRecord[projectName!][branch].commit
   );
   const currentCommit = dirBranchInfo.branches[branch].commit;
+  const lastBuildTime = safeData(
+    () => gitRecord[projectName!][branch].lastBuildTime
+  );
+  const currentTime = new Date().getTime();
+
   // 当前分支的 commit 等于之前存储记录的 commit，说明分支没发生变化
-  if (lastBuildCommit !== currentCommit) {
+  if (
+    lastBuildCommit !== currentCommit ||
+    (currentTime - lastBuildTime) / 1000 / 3600 / 24 > 3
+  ) {
     return true;
   } else {
     console.log(
